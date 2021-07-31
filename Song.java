2173372efd8df7e6dfeb10bd1e4588fa69bf95fe
stem league <64894084+stemleague@users.noreply.github.com>
2021-07-31 12:31:38 -0700

    Create Song.java

---
+++ Song.java
@@ -0,0 +1,40 @@
+public class Song {
+  public static void main(String args[]) {
+    verseOne();
+    chorus();
+    verseTwo();
+    chorus();
+  }
+
+  public static void verseOne() {
+    System.out.println("I got my driver's license last week");
+    System.out.println("Just like we always talked about");
+    System.out.println("'Cause you were so excited for me");
+    System.out.println("To finally drive up to your house");
+    System.out.println("But today I drove through the suburbs");
+    System.out.println("Crying 'cause you weren't around");
+    System.out.println("And you're probably with that blonde girl");
+    System.out.println("Who always made me doubt");
+    System.out.println("She's so much older than me");
+    System.out.println("She's everything I'm insecure about");
+    System.out.println("Yeah, today I drove through the suburbs");
+    System.out.println("'Cause how could I ever love someone else?");
+  }
+
+  public static void chorus() {
+    System.out.println("And I know we weren't perfect but I've never felt this way for no one");
+    System.out.println("And I just can't imagine how you could be so okay now that I'm gone");
+    System.out.println("Guess you didn't mean what you wrote in that song about me");
+    System.out.println("'Cause you said forever, now I drive alone past your street");
+  }
+
+  public static void verseTwo() {
+    System.out.println("And all my friends are tired");
+    System.out.println("Of hearing how much I miss you, but");
+    System.out.println("I kinda feel sorry for them");
+    System.out.println("'Cause they'll never know you the way that I do, yeah");
+    System.out.println("Today I drove through the suburbs");
+    System.out.println("And pictured I was driving home to you");
+  }
+
+}
